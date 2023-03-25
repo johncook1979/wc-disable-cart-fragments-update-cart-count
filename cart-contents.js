2783282c0711acbd0a1e52362fffefd8a2fd06fa
johncook1979 <41190295+johncook1979@users.noreply.github.com>
2023-03-25 16:20:39 +1100

    Update cart-contents.js

--- cart-contents.js
+++ cart-contents.js
@@ -1,22 +1,30 @@
 (function($) {
-  // click cart icon show cart
+  // click cart icon to show cart contents. Remember to change the #mnu-item-name to your class or id name
   $(document).on("click", "#menu-item-name", function(e){ // edit the #menu-item-name to the id or class name used
     e.preventDefault();
-    $('#cart_container').toggleClass('visible');
-    $.ajax({
-      type: 'GET',
-      url: '/wp-content/themes/your-child-theme-name/cart-contents.php',
-      dataType: 'html',
-      timeout: 0,
-      success: function(data) {
-        $('#cart_container').html(data);
-        $('#cart-links').remove();
-      },
-      error: function(xhr, type){
-        alert('Ajax error!')
-      }
-
-    });
+    
+    // get the value of items in the cart. If the cart is empty it can be used to prevent an unnecessary call to the cart contents file
+    let cartCount = parseInt($(this).find('#cart_count').text()) || 0; // If NULL or NaN return 0
+    
+    // check if the cart count is greater than 1 and if so display the cart and get the contents
+    if(cartCount >= 1){
+      $('#cart_container').toggleClass('visible');
+      
+      // ajax call to cart-contents file
+      $.ajax({
+        type: 'GET',
+        url: '/wp-content/themes/your-child-theme-name/cart-contents.php',
+        dataType: 'html',
+        timeout: 0,
+        success: function(data) {
+          $('#cart_container').html(data);
+          $('#cart-links').remove();
+        },
+        error: function(xhr, type){
+          console.log('Ajax error!')
+        }
+      });
+    }
 
   });
 
